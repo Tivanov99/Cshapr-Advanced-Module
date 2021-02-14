6409f644e386775054e52ef3f43911fb5fb78a50
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:04:47 +0200

    Add files via upload

---
+++ Generics - Exercise/01. Generic Box of String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
