44eb68a4eb4bc03cebb2d334aecff7005698226e
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 10:45:52 +0200

    Add files via upload

---
+++ Defining Classes - Exercise/08. Car Salesman/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Engine
+    {
+        public Engine(string model, string power, string dispacement, string effinciency)
+        {
+            Model = model;
+            Power = power;
+            Displacement = dispacement;
+            Effinciency = effinciency;
+        }
+        public string Model { get; set; }
+        public string Power { get; set; }
+        public string Displacement { get; set; }
+        public string Effinciency { get; set; }
+
+    }
+}
