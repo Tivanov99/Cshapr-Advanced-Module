392f2332ebfd85ab4d724fc62c79451a65634def
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:09:26 +0200

    Add files via upload

---
+++ Generics - Exercise/05. Generic Count Method String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            int count = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+            string Value = Console.ReadLine();
+            Console.WriteLine(box.Swapper(list, Value));
+        }
+    }
+}
