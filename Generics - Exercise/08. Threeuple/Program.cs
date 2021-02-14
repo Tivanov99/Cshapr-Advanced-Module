8779156ce4314464ee35d291b2bf2d510b66e5a0
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:12:57 +0200

    Add files via upload

---
+++ Generics - Exercise/08. Threeuple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            string firstname = names[0] + " " + names[1];
+            string city = "";
+            for (int i = 3; i < names.Length; i++)
+            {
+                city += names[i]+ " ";
+            }
+            city.TrimEnd();
+            Threeuple<string, string, string> tupple = new Threeuple<string, string, string>(firstname, names[2], city);
+            Console.WriteLine(tupple.ToString());
+            names = Console.ReadLine().Split();
+            Threeuple<string, int, string> Second = new Threeuple<string, int, string>(names[0], int.Parse(names[1]), names[2]);
+            Console.WriteLine(Second.Drink());
+            names = Console.ReadLine().Split();
+            Threeuple<string, double, string> Third = new Threeuple<string, double, string>(names[0], double.Parse(names[1]), names[2]);
+            Console.WriteLine(Third.ToString());
+        }
+    }
+}
---
+++ Generics - Exercise/08. Threeuple/Threeuple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        private T1 item1;
+        private T2 item2;
+        private T3 item3;
+        public Threeuple(T1 first, T2 second, T3 three)
+        {
+            this.item1 = first;
+            this.item2 = second;
+            this.item3 = three;
+        }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2} -> {item3}";
+        }
+        public string Drink()
+        {
+            string result = "False";
+            if(item3.ToString()=="drunk")
+            {
+                result = "True";
+            }
+            return $"{item1} -> {item2} -> {result}";
+        }
+    }
+}
