253577be4c2dce0930dc68be837d145ccc03cd35
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:08:36 +0200

    Add files via upload

---
+++ Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Box
+    {
+        public void Swapper<T>(List<T> list, int[] indexes)
+        {
+            int FirstIndex = indexes[0];
+            int SecondIndex = indexes[1];
+            T temp = list[FirstIndex];
+            list[FirstIndex] = list[SecondIndex];
+            list[SecondIndex] = temp;
+            foreach (T item in list)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+    }
+}
---
+++ Generics - Exercise/04. Generic Swap Method Integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            int count = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swapper(list, indexes);
+        }
+    }
+}
