9fa2f5e468cb4a04821a8c553c41125f8fc1108a
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:11:03 +0200

    Add files via upload

---
+++ Generics - Exercise/07. Tuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            string firstname = names[0] + " " + names[1];
+            Tuple<string, string> tupple = new Tuple<string, string>(firstname, names[2]);
+            Console.WriteLine(tupple.ToString());
+            names = Console.ReadLine().Split();
+            Tuple<string, int> Second = new Tuple<string, int>(names[0], int.Parse(names[1]));
+            Console.WriteLine(Second.ToString());
+            names = Console.ReadLine().Split();
+            Tuple<int, double> Third = new Tuple<int, double>(int.Parse(names[0]), double.Parse(names[1]));
+            Console.WriteLine(Third.ToString());
+        }
+    }
+}
---
+++ Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Tuple<T1, T2>
+    {
+        private T1 item1;
+        private T2 item2;
+        public Tuple(T1 first, T2 second)
+        {
+            this.item1 = first;
+            this.item2 = second;
+        }
+        public override string ToString()
+        {
+            return $"{item1} -> {item2}";
+        }
+    }
+}
