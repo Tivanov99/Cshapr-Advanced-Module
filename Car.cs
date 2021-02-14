1257e1c2aacd0acbc21212484964dc7d6519e754
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 10:36:01 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        string make;
+        string model;
+        int year;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
