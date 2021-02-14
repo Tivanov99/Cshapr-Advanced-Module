a9ee5995b91babe995cdedad5a531cea7bf2c87a
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-14 11:10:13 +0200

    Add files via upload

---
+++ Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericBox
+{
+    public class Box
+    {
+        public int Swapper<T>(List<T> list, double value)
+            where T : IComparable
+        {
+            int count = 0;
+            foreach (T item in list)
+            {
+                if(item.CompareTo(value)==1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Generics - Exercise/06. Generic Count Method Double/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            int count = int.Parse(Console.ReadLine());
+            List<double> list = new List<double>();
+            for (int i = 0; i < count; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            double Value = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Swapper(list, Value));
+        }
+    }
+}
