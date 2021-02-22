2b391eb1266b36d55a5bec74e64d6e40c2ee0648
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 18:55:50 +0200

    Add files via upload

---
+++ Iterators and Comparators - Lab/04. BookComparer/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            List<Book> copy = books.ToList();
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return copy[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+
+            return GetEnumerator();
+        }
+
+    }
+}
+
