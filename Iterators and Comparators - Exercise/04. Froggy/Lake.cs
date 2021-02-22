ee6d9936635f10c3f4eb9e930012948b83d869b6
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 19:03:21 +0200

    Add files via upload

---
+++ Iterators and Comparators - Exercise/04. Froggy/Lake.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise
+{
+    public class Lake : IEnumerable<int>
+    {
+        int[] stones;
+        public Lake(int[] input)
+        {
+            stones = new int[input.Length];
+            stones = input;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int EvenCount = 0;
+            int OddCount = 0;
+            for (int i = 0; i < stones.Length ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    EvenCount++;
+                }
+                else
+                {
+                    OddCount++;
+                }
+            }
+            int[] EvenPositions = new int[EvenCount];
+            int[] OddPositions = new int[OddCount];
+            EvenCount = 0;
+            OddCount = 0;
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    EvenPositions[EvenCount] = stones[i];
+                    EvenCount++;
+                }
+                else
+                {
+                    OddPositions[OddCount] = stones[i];
+                    OddCount++;
+                }
+            }
+            int[] reversed = new int[OddPositions.Length];
+            int index = 0;
+            for (int i = OddPositions.Length - 1; i >= 0; i--)
+            {
+                reversed[index] = OddPositions[i];
+                index++;
+            }
+            int ing = 0;
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (i < EvenPositions.Length)
+                {
+                    stones[i] = EvenPositions[i];
+                }
+                else
+                {
+                    stones[i] = reversed[ing];
+                    ing++;
+                }
+            }
+            for (int i = 0; i < stones.Length; i++)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int length = input.Length;
+            Lake lake = new Lake(input);
+            foreach (int item in lake)
+            {
+                length--;
+                if (length == 0)
+                {
+                    Console.Write(item+ " ");
+                }
+                else
+                {
+                    Console.Write(item + ", ");
+                }
+            }
+        }
+    }
+}
