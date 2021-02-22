b44eae4da7e715fd181d4ea0b4884a0ba656fe28
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 18:58:46 +0200

    Add files via upload

---
+++ Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Exercise
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> MyList;
+        private int index;
+        public ListyIterator(params T[] input)
+        {
+            MyList = new List<T>(input);
+        }
+
+        public bool Move()
+        {
+            if (index < MyList.Count-1)
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            return index+1 < MyList.Count;
+        }
+        public void Print()
+        {
+            if (MyList.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(MyList[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in MyList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/01. ListyIterator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string[] coppy = new string[input.Length - 1];
+            for (int i = 1; i < input.Length; i++)
+            {
+                coppy[i - 1] = input[i];
+            }
+            ListyIterator<string> listy = new ListyIterator<string>(coppy);
+
+            while (true)
+            {
+                string[] Comands = Console.ReadLine().Split();
+                if (Comands[0] == "END")
+                {
+                    break;
+                }
+                else if (Comands[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+                else if (Comands[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (Comands[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+            }
+        }
+    }
+}
