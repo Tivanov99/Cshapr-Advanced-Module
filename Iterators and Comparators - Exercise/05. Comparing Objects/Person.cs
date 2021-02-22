62af55850c2d4f6b4be7b88ed2de0251f302be6f
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 19:04:25 +0200

    Add files via upload

---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Exercise
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(params string[] input)
+        {
+            Name = input[0];
+            Age = int.Parse(input[1]);
+            Town = input[2];
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            int result = 1;
+            if (other != null)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+            if (result == 0)
+            {
+                return this.Age.CompareTo(other.Age);
+                if (result == 0)
+                {
+                    return this.Town.CompareTo(other.Town);
+                }
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> coll = new List<Person>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                if (input[0] == "END")
+                {
+                    break;
+                }
+                Person person = new Person(input);
+                coll.Add(person);
+            }
+            int num = int.Parse(Console.ReadLine());
+            Person comparePerson = coll[num - 1];
+            int samePll = 0;
+            foreach (Person item in coll)
+            {
+                if (item.CompareTo(comparePerson) == 0)
+                {
+                    samePll++;
+                }
+            }
+            if (samePll == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{samePll} {coll.Count-samePll} {coll.Count}");
+            }
+        }
+    }
+}
