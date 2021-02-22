2345401955b9dc0100b723b668fbed10a7b73fd3
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 19:02:20 +0200

    Add files via upload

---
+++ Iterators and Comparators - Exercise/03. Stack/CustumeStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise
+{
+    public class CustumeStack<T> : IEnumerable<T>
+    {
+        public Stack<T> stack;
+        public CustumeStack()
+        {
+            stack = new Stack<T>();
+        }
+        public void Push(T element)
+        {
+            stack.Push(element);
+        }
+        public void Pop()
+        {
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in stack)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Replace("Push", ", ");
+            string[] da = input.Split(", ");
+            CustumeStack<int> stack = new CustumeStack<int>();
+            for (int i = 1; i < da.Length; i++)
+            {
+                stack.Push(int.Parse(da[i]));
+            }
+            while (true)
+            {
+                string[] comand = Console.ReadLine().Split();
+                if (comand[0] == "END")
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        foreach (var item in stack)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    break;
+                }
+                else if (comand[0] == "Pop")
+                {
+                    stack.Pop();
+                }
+                else if (comand[0] == "Push")
+                {
+                    int num = int.Parse(comand[1]);
+                    stack.Push(num);
+                }
+            }
+
+        }
+    }
+}
