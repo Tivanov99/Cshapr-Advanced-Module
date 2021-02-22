ba108bca238817e93b3194804322a4f6ddebd702
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-02-22 19:05:24 +0200

    Add files via upload

---
+++ Iterators and Comparators - Exercise/06. Equality Logic/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Exercise
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(params string[] input)
+        {
+            Name = input[0];
+            Age = int.Parse(input[1]);
+            IsTheSame = false;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+        public bool IsTheSame { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = 1;
+            if (other != null)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+            if (result == 0)
+            {
+                if (this.Age.CompareTo(other.Age) == 0)
+                {
+                    IsTheSame = true;
+                }
+                else
+                {
+                    return this.Age.CompareTo(other.Age);
+                }
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/06. Equality Logic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> set = new SortedSet<Person>();
+            HashSet<Person> hash = new HashSet<Person>();
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                Person person = new Person(input);
+                    set.Add(person);
+                if (person.IsTheSame != true)
+                {
+                    hash.Add(person);
+                }
+            }
+            Console.WriteLine(set.Count());
+            Console.WriteLine(hash.Count());
+
+        }
+    }
+}
