e0b60e25b6d1dd4a78290563f1a5673cb0800563
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:44:56 +0300

    Add files via upload

---
+++ ADVANCED/Design Patterns/AbstractFactory/Contracts/ISmartPhone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Contracts
+{
+    public interface ISmartPhone
+    {
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ ADVANCED/Design Patterns/AbstractFactory/Iphone/IphoneTablet.cs
@@ -0,0 +1,18 @@
+﻿using AbstractFactory.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Iphone
+{
+    public class IphoneTablet : ITablet
+    {
+        public IphoneTablet(string model, int price)
+        {
+            Model = model;
+            Price = price;
+        }
+        public string Model { get ; set ; }
+        public int Price { get ; set ; }
+    }
+}
