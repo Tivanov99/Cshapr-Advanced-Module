96977f6edf1e069342bd68bfe25eef5c3f0a10eb
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:25:46 +0300

    Add files via upload

---
+++ Unit Testing - Lab/01. Test Axe/Fakes/FakeDummy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeAxeAndDummy
+{
+    public class FakeDummy : ITarget
+    {
+        public int Health => 0;
+
+        public int GiveExperience()
+        {
+            return 20;
+        }
+
+        public bool IsDead()
+        {
+            return true;
+        }
+
+        public void TakeAttack(int attackPoints)
+        {
+            
+        }
+    }
+}
---
+++ Unit Testing - Lab/01. Test Axe/HeroTests.cs
@@ -0,0 +1,33 @@
+﻿using FakeAxeAndDummy;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class HeroTests
+{
+    private FakeWeapon fakeWeapon;
+    private FakeDummy fakeDummy;
+    private Hero hero;
+    [SetUp]
+    public void SetUp()
+    {
+        fakeWeapon = new FakeWeapon();
+        fakeDummy = new FakeDummy();
+    }
+    [Test]
+    public void When_TargetIsDeadHero_ShouldReceiveXP()
+    {
+        Mock<ITarget> target = new Mock<ITarget>();
+        target.Setup(w => w.Health).Returns(0);
+        target.Setup(t => t.IsDead()).Returns(true);
+        target.Setup(t => t.GiveExperience()).Returns(15);
+        Mock<IWeapon> weapon = new Mock<IWeapon>();
+        weapon.Setup(w => w.AttackPoints).Returns(10);
+        weapon.Setup(w => w.DurabilityPoints).Returns(20);
+
+        hero = new Hero("trix", weapon.Object);
+        hero.Attack(target.Object);
+        Assert.AreEqual(15, hero.Experience);
+
+    }
+}
