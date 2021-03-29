fb3e18824dda722805ccce32817f540167d9ec5c
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:55:47 +0300

    Add files via upload

---
+++ ADVANCED/Design Patterns/Visitor/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    class Director : Employee
+    {
+        // Constructor
+
+        public Director()
+          : base("Elly", 35000.0, 16)
+        {
+        }
+    }
+}
---
+++ ADVANCED/Design Patterns/Visitor/President.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    class President : Employee
+    {
+        // Constructor
+
+        public President()
+          : base("Dick", 45000.0, 21)
+        {
+        }
+    }
+}
