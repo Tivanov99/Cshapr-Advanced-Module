69619f70c29ff61049b6ac7aae839ee710b4c950
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 21:01:00 +0300

    Add files via upload

---
+++ ADVANCED/Set_Cover_Problem/SetCover.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SetCover
+{
+    public static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    }
+}
