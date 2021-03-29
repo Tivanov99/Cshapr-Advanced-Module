5fa511e71411ae3707a541be40ac094fc66bff0f
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:49:19 +0300

    Add files via upload

---
+++ ADVANCED/Design Patterns/CompositePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("Phone", 256);
+            phone.CalculateTotalPrice();
+            Console.WriteLine();
+            //composite gift
+            var rootBox = new CompositeGift("RootBox", 0);
+            var truckToy = new SingleGift("TruckToy", 289);
+            var plainToy = new SingleGift("PlainToy", 587);
+          
+            rootBox.Add(plainToy);  rootBox.Add(truckToy);
+            var childBox = new CompositeGift("ChildBox", 0);
+            var soldierToy = new SingleGift("SoldierToy", 200);
+            childBox.Add(soldierToy);
+            rootBox.Add(childBox);
+            Console.WriteLine($"Total price of this composite present is: {rootBox.CalculateTotalPrice()}");
+        }
+    }
+}
