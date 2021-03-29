d57596a4d00491d9279f2b25d950e3863b940a63
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:58:02 +0300

    Add files via upload

---
+++ ADVANCED/CustomStack/CustomStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomeStack
+{
+    public class CustomStack
+    {
+        private const int InitialCapacity = 4;
+        private int[] items;
+        private int count;
+        public CustomStack()
+        {
+            count = 0;
+            items = new int[InitialCapacity];
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+        public void Push(int element)
+        {
+            if (items.Length == count)
+            {
+                Resize();
+            }
+            items[count] = element;
+            count++;
+        }
+        private void Resize()
+        {
+            int[] ResizedArray = new int[items.Length * 2];
+            for (int i = 0; i < count; i++)
+            {
+                ResizedArray[i] = items[i];
+            }
+            items = ResizedArray;
+        }
+        public int Pop()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+            int lastItem = items[count - 1];
+            int[] coppy = new int[count - 1];
+            for (int i = 0; i < count; i++)
+            {
+                coppy[i] = items[i];
+            }
+            items = coppy;
+            return lastItem;
+        }
+        public int Peek()
+        {
+            return items[count - 1];
+        }
+        public void ForEach(Action<object>action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action(items[i]);
+            }
+        }
+    }
+}
---
+++ ADVANCED/CustomStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomeStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+            stack.Push(1);
+            stack.Push(95);
+            stack.Push(83);
+            stack.Push(37);
+            stack.Push(92);
+            stack.Push(19);
+            stack.Push(24);
+            stack.Push(62);
+            stack.Peek();
+            stack.Pop();
+            
+        }
+    }
+}
