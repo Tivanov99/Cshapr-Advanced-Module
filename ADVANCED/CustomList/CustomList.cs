8c8cb0a06ea1a68045096f1b24805d192819ad1e
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-29 20:58:48 +0300

    Add files via upload

---
+++ ADVANCED/CustomList/CustomList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return items[index];
+            }
+            set
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                items[index] = value;
+            }
+        }
+        private void Resize()
+        {
+            int[] ResizedArray = new int[items.Length * 2];
+            for (int i = 0; i < Count; i++)
+            {
+                ResizedArray[i] = items[i];
+            }
+            items = ResizedArray;
+        }
+        public void Add(int element)
+        {
+            if (Count == items.Length)
+            {
+                Resize();
+            }
+            items[Count] = element;
+            Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int ValueOfIndex = items[index];
+            items[index] = default(int);
+            Shift(index);
+            Count--;
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+            return ValueOfIndex;
+        }
+        private void Shift(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+        private void Shrink()
+        {
+            int[] ShrinkedArry = new int[items.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                ShrinkedArry[i] = items[i];
+            }
+            items = ShrinkedArry;
+        }
+        public void Insert(int index, int item)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+            int MiddleofArray = items.Length / 2;
+            ShiftToLeft(index, item);
+            Count++;
+        }
+        private void ShiftToLeft(int index, int element)
+        {
+            int[] NewArray = new int[items.Length];
+            for (int i = 0; i < index; i++)
+            {
+                NewArray[i] = items[i];
+            }
+            NewArray[index] = element;
+            for (int i = index; i <= Count; i++)
+            {
+                NewArray[i + 1] = items[i];
+            }
+            items = NewArray;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int FirstIndex, int Secoundindex)
+        {
+            if (FirstIndex < 0 || FirstIndex > Count || Secoundindex < 0 || Secoundindex > Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int tempofFirst = items[FirstIndex];
+            items[FirstIndex] = items[Secoundindex];
+            items[Secoundindex] = tempofFirst;
+        }
+    }
+}
---
+++ ADVANCED/CustomList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+            list.Add(5);
+            list.Add(10);
+            list.Add(30);
+            list.Add(3);
+            list.Add(1);
+            list.Add(100);
+            list.Add(75);
+            list.Add(9);
+            list.Add(38);
+            list.Add(64);
+            list.Add(999);
+
+          
+            list.Swap(5, 10);
+        }
+    }
+}
