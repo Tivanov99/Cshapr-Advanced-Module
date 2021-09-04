b3721c85daf58da12e1b5e671207f26b4311d3b9
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-09-04 22:07:23 +0300

    Add files via upload

---
+++ ADVANCED/Exercises: Trees Representation and Traversal (BFS and DFS)/Tree.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Tree
+{
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> _childrens;
+        public Tree(T value)
+        {
+            this.Key = value;
+            this.Parent = null;
+            _childrens = new List<Tree<T>>();
+        }
+        public Tree(T value, Tree<T> child)
+            : this(value)
+        {
+            child.Parent = this;
+            this._childrens.Add(child);
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => this._childrens;
+
+        public void AddChild(Tree<T> child)
+        {
+            this._childrens.Add(child);
+            child.Parent = this;
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            if (parent != null)
+            {
+                this.Parent = parent;
+            }
+        }
+
+        public string GetAsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tree<T> GetDeepestLeftomostNode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetMiddleKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<T>> PathsWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tree<T>> SubTreesWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+        public Tree<T> FindNodeBFS(T key)
+        {
+            if (this.Key.Equals(key))
+            {
+                return this;
+            }
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+
+
+                foreach (Tree<T> child in queue.Peek().Children)
+                {
+                    if (child.Key.Equals(key))
+                    {
+                        return child;
+                    }
+                    queue.Enqueue(child);
+                }
+                queue.Dequeue();
+            }
+            return null;
+        }
+    }
+}
+
+
+  
+        
---
+++ ADVANCED/Exercises: Trees Representation and Traversal (BFS and DFS)/TreeFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tree
+{
+    public class TreeFactory
+    {
+        private Dictionary<int, Tree<int>> nodesBykeys;
+        Tree<int> Root;
+        public TreeFactory()
+        {
+            this.nodesBykeys = new Dictionary<int, Tree<int>>();
+        }
+
+        public Tree<int> CreateTreeFromStrings(string[] input)
+        {
+            foreach (string item in input)
+            {
+                string[] Pair = item.Split(" ");
+                int ParentValue = int.Parse(Pair[0]);
+                int childValue = int.Parse(Pair[1]);
+                AddEdge(ParentValue, childValue);
+            }
+            return GetRoot();
+        }
+
+        public Tree<int> CreateNodeByKey(int key)
+        {
+            if (!nodesBykeys.ContainsKey(key))
+            {
+                nodesBykeys.Add(key, new Tree<int>(key));
+            }
+            return this.nodesBykeys.Values.FirstOrDefault(x => x.Key == key);
+        }
+
+        public void AddEdge(int parent, int child)
+        {
+            Tree<int> Parent = CreateNodeByKey(parent);
+            Tree<int> Child = CreateNodeByKey(child);
+            if (Parent.Parent == null)
+            {
+                this.Root = Parent;
+            }
+
+            Parent.AddChild(Child);
+            Child.AddParent(Parent);
+        }
+
+        private Tree<int> GetRoot()
+        {
+            return this.Root;
+        }
+    }
+}
+
+
+        
+  
