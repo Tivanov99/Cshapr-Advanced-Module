98fe9c3da2823e425484ca9be216d2521f669bfb
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-09-07 22:13:25 +0300

    Add files via upload

---
+++ ADVANCED/Max Heap/IAbstractHeap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxHeap
+{
+    interface IAbstractHeap<T> where T : IComparable<T>
+    {
+        int Size { get; }
+
+        void Add(T element);
+
+        T Peek();
+    }
+}
---
+++ ADVANCED/Max Heap/MaxHeap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxHeap
+{
+    public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
+    {
+        List<T> Heap;
+        public MaxHeap()
+        {
+            Heap = new List<T>();
+        }
+        public int Size => Heap.Count;
+
+        public void Add(T element)
+        {
+            Heap.Add(element);
+            HeapyifyUP(Heap.Count - 1);
+        }
+
+        public T Peek()
+        {
+            if (Heap.Count > 0)
+            {
+                return Heap[0];
+            }
+            throw new InvalidOperationException();
+        }
+        private void HeapyifyUP(int index)
+        {
+            int parentIndex = GetParentIndex(index);
+            while (index > 0 && isGreater(index, parentIndex))
+            {
+                Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = GetParentIndex(index);
+            }
+        }
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private void Swap(int index, int ParentIndex)
+        {
+            T temp = Heap[index];
+            Heap[index] = Heap[ParentIndex];
+            Heap[ParentIndex] = temp;
+        }
+
+        private bool isGreater(int index, int parentIndex)
+        {
+            if (Heap[index].CompareTo(Heap[parentIndex]) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
